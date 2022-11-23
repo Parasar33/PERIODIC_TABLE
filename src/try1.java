cda85d3958b3cefea3c6d5f1647cf4021852e42f
Rishav Parasar <rishavparasar2004@gmail.com>
2022-11-23 19:57:06 +0530

    Update try1.java

--- src/try1.java
+++ src/try1.java
@@ -3,5 +3,6 @@ public class try1
     public static void main(String[] args) {
         System.out.println("HELLO BEMTI");
         System.out.println("try again");
+        System.out.println("this change is made through github");
     }
 }
